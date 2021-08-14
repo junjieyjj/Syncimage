3b90943ae37d9f3e47f60cf8e5ecf5838ba51844
JackYap <junjieyjj@163.com>
2021-08-14 13:20:42 +0800

    init

---
+++ README.md
@@ -0,0 +1,174 @@
+# Ferryman
+[![](https://img.shields.io/github/workflow/status/junjieyjj/Ferryman/Ferryman)](https://github.com/junjieyjj/Ferryman/actions?query=workflow%3AFerryman)&nbsp;&nbsp;![](https://img.shields.io/badge/platform-Linux-blue)&nbsp;&nbsp;![Python 3.7](https://img.shields.io/badge/Python-v3.7-blue)&nbsp;&nbsp;![](https://img.shields.io/badge/Docker-v17.03.0+-blue)&nbsp;&nbsp;[![](https://img.shields.io/github/license/junjieyjj/Ferryman?color=red)](https://github.com/junjieyjj/Ferryman/blob/master/LICENSE)
+
+> &#8195;&#8195;国内因某些原因无法直接访问谷歌gcr.io导致无法直接获取K8s系列镜像，虽有部分云厂商提供K8s镜像，但可能不包含quay.io的系列镜像，且部分docker.io镜像即使配置国内镜像加速器地址还是速度缓慢，经测试阿里云的镜像加速器地址与个人免费镜像仓库镜像拉取速度对比，后者要比前者快2~3倍。
+>
+> &#8195;&#8195;基于以上背景，开发出该镜像同步工具，主要用于将镜像同步到国内个人仓库，从而获取被墙的镜像与提高镜像拉取速度。
+
+<br/>
+
+## **功能**
+
+- 全量：同步仓库下所有Tag
+
+- 限量：同步当下最新指定数量的Tag
+
+<br/>
+
+## 同步信息
+
+### 支持同步源
+
+> docker.io其实国内有很多镜像加速器，之所以支持docker.io是因为亲测阿里云的镜像加速器地址与个人免费镜像仓库镜像拉取对比，后者要比前者快一倍以上。
+
+- gcr.io
+- quay.io
+- docker.io
+
+<br/>
+
+### 同步列表
+
+> 本项目已托管到GitHub Actions，以下镜像列表每隔6小时自动同步更新一次；
+>
+> 已同步镜像的版本号列表可查看`history`目录下对应txt文件。
+
+| 镜像源     | 命名空间                      | 镜像                       |
+| ---------- | ----------------------------- | -------------------------- |
+| k8s.gcr.io |                               | kube-proxy                 |
+| k8s.gcr.io |                               | kube-scheduler             |
+| k8s.gcr.io |                               | kube-controller-manager    |
+| k8s.gcr.io |                               | kube-apiserver             |
+| k8s.gcr.io |                               | etcd                       |
+| k8s.gcr.io |                               | coredns                    |
+| k8s.gcr.io |                               | pause                      |
+| k8s.gcr.io |                               | kubernetes-dashboard-amd64 |
+| k8s.gcr.io |                               | metrics-server-amd64       |
+| k8s.gcr.io | ingress-nginx                 | controller                 |
+| k8s.gcr.io | descheduler                   | descheduler                |
+| quay.io    | coreos                        | flannel                    |
+| quay.io    | kubernetes-ingress-controller | nginx-ingress-controller   |
+| quay.io    | coreos                        | kube-state-metrics:v1.9.7  |
+| docker.io  | kubernetesui                  | dashboard                  |
+| docker.io  | jenkins                       | jenkins                    |
+| docker.io  | jenkinsci                     | blueocean                  |
+| docker.io  | sonatype                      | nexus3                     |
+| docker.io  | library                       | docker                     |
+| docker.io  | gitlab                        | gitlab-ce                  |
+| docker.io  | gitlab                        | gitlab-runner              |
+| docker.io  | jumpserver                    | jms_all                    |
+| docker.io  | library                       | sonarqube                  |
+| docker.io  | library                       | traefik                    |
+| docker.io  | library                       | nginx                      |
+| docker.io  | library                       | tomcat                     |
+| docker.io  | library                       | openjdk                    |
+| docker.io  | library                       | mysql                      |
+| docker.io  | library                       | redis                      |
+| docker.io  | jettech                       | kube-webhook-certgen       |
+| docker.io  | library                       | busybox                    |
+
+
+
+<br/>
+
+### 同步源对应地址
+
+| 源         | 目标                                                         |
+| ---------- | ------------------------------------------------------------ |
+| k8s.gcr.io | **registry.cn-shenzhen.aliyuncs.com/kubernetes_aliyun/**{image}:{tag} |
+| quay.io    | **registry.cn-shenzhen.aliyuncs.com/quayio_aliyun/**{image}:{tag} |
+| docker.io  | **registry.cn-shenzhen.aliyuncs.com/dockerio_aliyun/**{image}:{tag} |
+
+**镜像拉取示例**
+
+**前**：docker pull k8s.gcr.io/pause:2.0
+
+**后**：docker pull registry.cn-shenzhen.aliyuncs.com/kubernetes_aliyun/pause:2.0
+
+
+
+<br/>
+
+## 文件说明
+
+- ### ferryman.py
+
+  - 主脚本
+  
+- ### items.yml
+
+  - 存放需要同步的镜像列表，可根据自身需求修改。
+  - `limit`字段用于限制每次更新版本数量，默认为9999，可根据自身需求修改。
+
+- ### history（目录）
+
+  - 存放每个镜像的同步记录，程序会跳过已同步记录，首次使用时请先删除整个目录。
+
+<br/>
+
+## 使用说明
+
+1. **配置私有镜像仓库账号密码**
+
+   编辑`ferryman.py`修改以下内容为你的个人镜像仓库账号密码
+
+   ```shell
+   target_auth = {'username': 'Your username', 'password': 'Your password'}
+   ```
+
+   **或**
+
+   使用临时环境变量配置你的个人镜像仓库账号密码
+
+   ```shell
+   export TARGET_USER='Your username'
+   export TARGET_PASSWORD='Your password'
+   ```
+
+2. **配置私有镜像仓库地址**
+
+   编辑`items.yml`修改每个项目`target`指向你的个人镜像仓库（尾部要以“/”结束）
+
+   ```yaml
+   kube-apiserver:
+     source: k8s.gcr.io/
+     target: registry.cn-shenzhen.aliyuncs.com/kubernetes_aliyun/
+   ```
+
+3. **删除本仓库的同步记录**：删除`history`目录，否则程序运行时会读取并跳过已同步的历史记录
+
+4. **运行程序**：`python ferryman.py`
+
+<br/>
+
+## 更新记录
+
+- **2020/05/10**    初始化
+
+- **2020/05/11**    重新封装docker-py的push方法，增加登录仓库验证失败提示与显示推送进度
+
+- **2020/05/17**    重写核心部分，不向后兼容
+  
+
+<br/>
+
+## 已知问题
+
+1. 当前项目配置文件因为结构问题，所以暂不支持存在相同项目名，待支持。
+
+<br/>
+
+## 流程图
+
+> 以下图片可能因GWF原因无法正常显示
+
+![流程图](https://s1.ax1x.com/2020/05/15/YrIXXq.png)
+
+## 相似项目指引
+
+- https://github.com/deislabs/oras
+- https://github.com/containers/skopeo
+- https://github.com/containers/image
+- https://github.com/AliyunContainerService/image-syncer
+
+trigger
---
+++ ferryman.py
@@ -0,0 +1,361 @@
+#!/usr/bin/python
+import os,datetime,logging,json,re
+import requests
+import yaml
+import docker
+from retry.api import retry_call
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    # level=logging.DEBUG,
+    format='[%(asctime)s] [%(levelname)s] [%(funcName)s] %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
+
+pydocker = docker.APIClient(timeout=60, base_url='unix:///var/run/docker.sock')
+
+#################################################
+
+# 目标私人仓库认证
+# 优先从环境变量TARGET_USER、TARGET_PASSWORD获取私人仓库账号密码，用于支持GitHub Actions
+if os.environ.get('TARGET_USER') and os.environ.get('TARGET_PASSWORD'):
+    target_user = os.environ.get('TARGET_USER')
+    target_password = os.environ.get('TARGET_PASSWORD')
+    target_auth = { 'username': target_user, 'password': target_password }
+else:
+    target_auth = {'username': 'Your username', 'password': 'Your password'}
+
+
+# 重新封装Docker模块Push方法，增加登录验证失败报错与推送镜像时显示进度条
+def docker_push(image, auth):
+    output = pydocker.push(image, auth_config=auth, stream=True, decode=True)
+    for line in output:
+        if line.get("error"):
+            raise InterruptedError(line.get("error").replace('\n', ' '))
+        if line.get("progress"):
+            print(line.get("status"), line.get("progress"), end="\r")
+    return
+
+
+# 创建目录
+def create_dir(dir_path):
+    if os.path.exists(dir_path):
+        logging.info (f"The {dir_path} directory already exists")
+    else:
+        logging.info (f"No directory found for {dir_path}")
+        os.makedirs(dir_path)
+        logging.info (f"Create {dir_path} directory")
+    return
+
+
+# 加载同步项目清单（YML文件）
+def load_yml(file):
+    logging.info (f"Load YML File: {file}")
+    with open(file, "rb") as f:
+        result = yaml.load(f, Loader=yaml.SafeLoader)
+        logging.debug (f"Load YML File:  {result}")
+    return result
+
+
+# 读取本地历史更新记录
+def load_history(items, domain):
+    file = (f"./history/{domain}__{items}.txt")
+    result = []
+    if os.path.exists(file):
+        logging.debug (f"Discover local history files: {file}")
+        with open(file, 'r', encoding='utf-8') as f:
+            for line in f:
+                line = line.strip('\n').split("\t\t")
+                result.append({f"dt": line[0], "sha256": line[1], "tag": line[2]})
+        logging.debug (f"Local List: Count {len(result)}, {result}")
+    else:
+        logging.debug (f"Can't find local history file: {file}")
+    return result
+
+
+# 回写增量同步记录到本地历史记录
+def write_history(items, domain, local_list):
+    file = (f"./history/{domain}__{items}.txt")
+    f = open(file, 'w', encoding='utf-8')
+    for i in local_list:
+        f.write(f'{i["dt"]}\t\t{i["sha256"]}\t\t{i["tag"]}\n')
+    f.close()
+    logging.info ("Append tags to local history")
+    return
+
+
+# 日期时间转换，输出统一格式日期
+def datetime_conv(dt):
+    dt = str(dt)
+    if dt.isdigit():
+        if len(dt) > 10:
+            dt = int(dt)/1000
+        d = datetime.datetime.fromtimestamp(int(dt))
+    else:
+        d = datetime.datetime.strptime(dt, '%Y-%m-%dT%H:%M:%S.%fZ')
+    result = d.strftime("%Y-%m-%d %H:%M:%S")
+    return result
+
+
+# 获取k8s的tag
+def requests_gcr(items, namespaces = "none"):
+    logging.info (f"The current mirror source is k8s.gcr.io")
+    # 获取Tag
+    if namespaces == "none":
+        url = (f"https://k8s.gcr.io/v2/{items}/tags/list")
+    else:
+        url = (f"https://k8s.gcr.io/v2/{namespaces}/{items}/tags/list")
+    r = requests.get(url)
+    response_dict = r.json().get("manifest")
+    # 提取Tag、Date、sha256
+    src_list = []
+    for v in response_dict.items():
+        if len(v[1]["tag"]) != 0:
+            tag = v[1]["tag"][0]
+            sha256 = v[0]
+            dt = datetime_conv(v[1]["timeUploadedMs"])
+            src_list.append({"dt": dt, "sha256": sha256, "tag": tag})
+    # 按时间字段排序
+    result = sorted(src_list, key=lambda x: x["dt"],reverse=True)
+    return result
+
+
+# 获取quay.io的tag
+def requests_quay(items, namespaces = "none"):
+    logging.info (f"The current mirror source is quay.io")
+    # 循环获取Tag
+    response_list = []
+    for i in range(100):
+        page = (i + 1)
+        url = (f"https://quay.io/api/v1/repository/{namespaces}/{items}/tag?limit=100&page={page}&onlyActiveTags=true")
+        r = requests.get(url)
+        buffer_list = r.json().get("tags")
+
+        if len(buffer_list) != 0:
+            logging.info (f"The { page } page, tags count {len (buffer_list)}")
+            for x in buffer_list:
+                response_list.append(x)
+        else:
+            logging.info (f"The { page } page, this is the last page")
+            break
+    # 提取Tag、Date、sha256
+    src_list = []
+    for i in response_list:
+        tag = i["name"]
+        sha256 = i["manifest_digest"]
+        if "start_ts" in i.keys():
+            dt = datetime_conv(i["start_ts"])
+        else:
+            dt = datetime_conv(0)
+        src_list.append({"dt": dt, "sha256": sha256, "tag": tag})
+    # 按时间字段排序
+    result = sorted(src_list, key=lambda x: x["dt"],reverse=True)
+    return result
+
+
+# 获取dockio.io的tag
+def requests_docker(items, namespaces = "none"):
+    logging.info (f"The current mirror source is docker.io")
+    # 循环获取Tag
+    response_list = []
+    for i in range(100):
+        page = (i + 1)
+        url = (f"https://hub.docker.com/v2/repositories/{namespaces}/{items}/tags?page_size=100&page={page}")
+        r = requests.get(url)
+        buffer_list = r.json().get("results")
+
+        if len(buffer_list) != 0:
+            logging.info (f"The { page } page, tags count {len (buffer_list)}")
+            for x in buffer_list:
+                response_list.append(x)
+        else:
+            logging.info  (f"The { page } page, this is the last page")
+            break
+    # 提取Tag和Date
+    src_list = []
+    for v in response_list:
+        tag = v["name"]
+        # Get datetime
+        if v['last_updated'] == None:
+            dt = datetime_conv(0)
+        else:
+            dt = datetime_conv(v["last_updated"])
+        # Get sha256
+        for x in v['images']:
+            if x['architecture'] == "amd64" and x['os'] != "windows":
+                if "digest" in x.keys():
+                    sha256 = x['digest']
+                else:
+                    sha256 = "Unknown"
+                src_list.append({"dt": dt, "sha256": sha256, "tag": tag})
+    # 按时间字段排序
+    result = sorted(src_list, key=lambda x: x["dt"],reverse=True)
+    return result
+
+
+# 定义镜像缓存队列
+queue_list = []
+# 清理缓存镜像
+def cache_cleanup():
+    global queue_list
+    for i in range(len(queue_list)):
+        logging.debug (f"Queue count: {len(queue_list)}，Current clean image: {queue_list[0]}")
+        pydocker.remove_image(queue_list[0])
+        queue_list.pop(0)
+        if len(queue_list) == 0:
+            logging.debug (f"Queue count: {len(queue_list)}")
+    return
+
+
+# 镜像同步
+def sync_images(image, src_repo, target_repo, tag_list):
+    try:
+        for num, item in enumerate(tag_list, start=1):
+            total = len(tag_list)
+            tag = item['tag']
+            dt = item['dt']
+            sha256 = item['sha256']
+
+            source = src_repo + image + ":" + tag
+            target = target_repo + image + ":" + tag
+            domain = src_repo.split("/")[0]
+
+            starttime = datetime.datetime.now()
+            logging.info (f"({num}/{total}) {image}:{tag}")
+            logging.info (f"Image Update: {dt}")
+
+            logging.info (f"Pull Image: {image}, Tag: {tag}")
+            retry_call(pydocker.pull, fargs=[source], exceptions=Exception, tries=6, delay=10)
+
+            logging.info (f"Tag  Image: {image}, Tag: {tag}")
+            pydocker.tag(source, target)
+            #pydocker.remove_image(source)
+
+            logging.info (f"Push Image: {image}, Tag: {tag}")
+            retry_call(docker_push, fargs=[target, target_auth] , exceptions=Exception, tries=6, delay=10)
+            pydocker.remove_image(target)
+
+            # 加载本地历史同步记录
+            local_list = load_history(image, domain)
+            logging.debug (f"History list: Count {len(local_list)}, {local_list}")
+            # 以当前Tag去重后排序回写
+            local_list = [i for i in local_list if tag != i["tag"]]
+            local_list.append({"dt": dt, "sha256": sha256, "tag": tag})
+            local_list.sort(reverse=True, key=lambda x: x["dt"])
+            write_history(image, domain, local_list)
+
+            # 缓存镜像：缓存几个镜像在队列中，然后先进先出循环删除（利用缓存镜像加速相似镜像拉取速度） 
+            global queue_list
+            queue_list.append(source)
+            if len(queue_list) == 6:
+                logging.debug (f"Count before clearing the queue: {len(queue_list)}，Queue list: {queue_list}")
+                logging.debug (f"Current clean image: {queue_list[0]}")
+                pydocker.remove_image(queue_list[0])
+                queue_list.pop(0)
+                logging.debug (f"Count after clearing the queue: {len(queue_list)}，Queue list: {queue_list}")
+
+            endtime = datetime.datetime.now()
+            logging.info ("Execution time: %s\n" %(endtime - starttime))
+    except (Exception, KeyboardInterrupt) as e:
+        # 缓存镜像：结束前删除队列中剩余镜像
+        # cache_cleanup()
+
+        logging.error (e)
+        logging.warning ("%s Sync Failed, Abnormal Exit!" % image)
+        exit(1)
+    else:
+        # 缓存镜像：结束前删除队列中剩余镜像
+        # cache_cleanup()
+
+        logging.info ("%s Sync Success" % image)
+    return
+
+
+# 主逻辑
+def main(yml):
+    # 获取同步项目清单
+    for (key, value) in yml.items():
+        items = key
+        source = value["source"]
+        domain = source.split("/")[0]
+        namespaces = source.split("/")[-2]
+        target = value["target"]
+        limit = value["tag"]["limit"]
+        
+        # 开始同步
+        logging.info (f"Start syncing: {items}")
+        logging.info (f"Update limit: {limit}")
+
+        # 判断源
+        if "k8s.gcr.io" in source:
+            if namespaces == domain:
+                src_list = requests_gcr(items)
+            else:
+                src_list = requests_gcr(items, namespaces)
+        elif 'quay.io' in source:
+            src_list = requests_quay(items, namespaces)
+        elif 'docker.io' in source:
+            src_list = requests_docker(items, namespaces)
+        else:
+            logging.info ("Unsupported sync source")
+            exit ()
+
+        # 排除掉Win平台、Gitlab-ce nightly
+        new_src_list = src_list.copy()
+        for i in new_src_list:
+            if "windowsservercore" in str(i) or "nanoserver" in str(i) or "nightly" in str(i)  or "windows" in str(i):
+                src_list.remove(i)
+        new_src_list.clear()
+
+        # 限制更新Tag数量
+        src_list = src_list[:limit]
+
+        logging.info (f"Latest version: {src_list[0]['tag']}, Lastst updated: {src_list[0]['dt']}")
+        logging.debug (f"Src list: Count {len(src_list)}, {src_list}")
+
+        # 加载本地同步记录
+        history_list = load_history(items, domain)
+
+        logging.debug (f"History list: Count {len(history_list)}, {history_list}")
+
+        # 递归获取源列表与本地列表差集
+        dedupl_list = src_list.copy()
+        for i in history_list:
+            for x in src_list:
+                if i["sha256"] in x.values() and i["tag"] in x.values():
+                    dedupl_list.remove(x)
+
+        # 按时间字段排序（降序）
+        dedupl_list.sort(reverse=True, key=lambda x: x['dt'])
+
+        logging.debug (f"Sync list: Count {len(dedupl_list)}, {dedupl_list}")
+        # 如果同步列表为空则不同步
+        if not dedupl_list:
+            logging.info (f"Src List: {len(src_list)}, History List: {len(history_list)}, Sync List: {len(dedupl_list)}")
+            logging.info (f"The Sync list of the {items} is empty, no need to sync.")
+            logging.info (f"{items} Sync Success")
+        else:
+            logging.info (f"Src List: {len(src_list)}, History List: {len(history_list)}, Sync List: {len(dedupl_list)}")
+            # 调用镜像同步
+            for i in items.splitlines(True):
+                sync_images(items, source, target, dedupl_list)
+        print ()
+    return
+
+
+# 初始化
+def init():
+    logging.info ("Start synchronization")
+
+    create_dir("history")
+    yml_file = load_yml("items.yml")
+    print ()
+    main(yml_file)
+
+
+# 入口
+if __name__ == "__main__":
+    init()
+
+
